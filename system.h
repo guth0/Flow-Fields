29009298c56da9648771484828eecbe792f8bdf0
Lucas Gutheim <lucas.gutheim@gmail.com>
2023-10-11 19:51:18 -0700

    Perlin Noise implimented

--- system.h
+++ system.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "particle.h"
-#include "vector_field.h"
+#include "noise.h"
 
 class ParticleSystem
 {
@@ -10,7 +10,8 @@ public:
 
     uint8_t standard_radius;
 
-    ParticleSystem()
+    ParticleSystem(uint16_t seed_)
+        : m_grid{seed_}
     {
     }
 
@@ -44,9 +45,9 @@ public:
         m_frame_dt = 1.0f / static_cast<float>(rate);
     }
 
-    void resizeGrid(uint16_t cell_size)
+    void resizeGrid(uint16_t height, uint16_t width, uint16_t cell_size)
     {
-        m_grid.setSize(cell_size);
+        m_grid.setSize(width + 2, height + 2); // add buffers on each side
         m_cell_size = cell_size;
     }
 
@@ -63,7 +64,7 @@ public:
     void setWorldSize(sf::Vector2i size)
     {
         m_world_size = size;
-        m_grid.window_resolution = size;
+        // m_grid.window_resolution = size;
     }
 
     void setParticleVelocity(Particle &particle, sf::Vector2f v)
@@ -71,9 +72,9 @@ public:
         particle.setVelocity(v, getStepDt());
     }
 
-    void generateField(float zoom, float curve, float offset)
+    void generateField()
     {
-        m_grid.createField(zoom, curve, offset);
+        m_grid.generateField();
     }
 
     [[nodiscard]] const std::vector<Particle> &getParticles() const
@@ -97,16 +98,19 @@ public:
     }
 
 private:
-    uint32_t m_num_substep = 1; // dont know if substeps are needed
+    uint32_t m_num_substep = 1; // dont think substeps are needed
     sf::Vector2f m_center;
     std::vector<Particle> m_particles;
 
-    float speed_coefficent = 200;
+    static constexpr uint16_t speed_coefficent1 = 20;
+    static constexpr float slow_ratio = 0.1f; // slow down ratio%
+
+    static constexpr uint16_t speed_coefficent2 = 200;
 
     float m_time = 0.0f;
     float m_frame_dt = 0.0f;
 
-    VectorField m_grid;
+    PerlinField m_grid;
     uint16_t m_cell_size;
 
     void updateParticles(float dt)
@@ -150,6 +154,7 @@ private:
             {
 
                 // get position of cell that particle is in
+                // the "+1"s are for the buffers of the grid
                 uint32_t cell_position = floor(particle.position.x / m_cell_size) + floor(particle.position.y / m_cell_size + 1) * m_grid.width + 1;
 
                 float x = cos(m_grid.data[cell_position]);
@@ -158,15 +163,12 @@ private:
                 sf::Vector2f vec = sf::Vector2f(x, y);
                 // apply that cell's vector to the particle
 
-                // smoother turs
-                // particle.addVelocity(vec, dt);
+                //// smoother turs ////
+                particle.addVelocity(vec * (float)speed_coefficent1, dt);
+                particle.slowDown(slow_ratio);
 
-                // particle.slowdown(.001f);
-
-                // sharper turns
-                particle.setVelocity(vec * speed_coefficent, dt);
-
-                // both .addVelocity() and .setVelocity() work
+                //// sharper turns ////
+                // particle.setVelocity(vec * (float)speed_coefficent2, dt);
             }
         }
     }
